e1d0b13ce83838841817e27fd4cc1df4ff198435
jaja1927 <rizqijazuliabdilah@gmail.com>
2023-11-08 06:32:55 +0700

    update

---
+++ assets/js/custom.js
@@ -0,0 +1,17 @@
+//  $(document).ready(function () {
+//     $("#pre").fadeOut("slow");
+    
+//    console.log(Document.getElementById("pre"))
+//  });
+
+
+
+
+let order = () => alert("Orderan Di Terima!!");
+let contact = () => alert("Pesan Di Kirim");
+
+
+var loader = document.getElementById("pre");
+window.addEventListener("load", function () {
+loader.style.display ="none";
+})
--- main.js
+++
@@ -1,2 +0,0 @@
-let order = () => alert("Orderan Di Terima!!");
-let contact = () => alert("Pesan Di Kirim");
